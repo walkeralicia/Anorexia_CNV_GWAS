7d22880df4e1d087ee7c1f92b0278e367363907d
walkeralicia <alicia.walker@psych.ox.ac.uk>
2024-03-18 17:10:10 +0000

    CNV_plot

--- 03_CNV_Burden/S2_genome_wide/S2_totalCNV_burden.R
+++ 03_CNV_Burden/S2_genome_wide/S2_totalCNV_burden.R
@@ -188,14 +188,14 @@ dat$Type <- ifelse(dat$Type=="Deletions and Duplications", "Both CNV Types",
 dat$Type <- factor(dat$Type, levels=c("Deletion-only", "Duplication-only", "Both CNV Types"))
 
 p1 <- ggplot(data=dat, aes(x=OR, y=Length, color=Type)) +
-  geom_errorbar(aes(xmin=lower, xmax = upper), width=0.4, linewidth = 6, position = position_dodge(width = 0.6)) +
-  geom_point(size=12, position = position_dodge(width = 0.6))  +
+  geom_errorbar(aes(xmin=lower, xmax = upper), width=0.4, linewidth = 8, position = position_dodge(width = 0.6)) +
+  geom_point(size=16, position = position_dodge(width = 0.6))  +
   labs(x='OR', y = 'CNV Length', tag = "B") +
   geom_vline(xintercept=1, color='black', linetype='dashed', alpha=2, linewidth=1.5) +
   xlim(0.5, 1.7) +
-  scale_color_manual(values = c("Both CNV Types" = "#AF58BA", "Duplication-only" = "#F28522", "Deletion-only" = "#009E73")) + 
+  scale_color_manual(values = c("Both CNV Types" = "purple", "Duplication-only" = "#F28522", "Deletion-only" = "blue")) + 
   scale_y_discrete(limits = c("All", "20-100kB", "100-200kB", "200-500kB", ">500kB")) +
-  theme_minimal(base_size=110) +
+  theme_minimal(base_size=120) +
   scale_x_continuous(breaks = c(0.75, 1, 1.25))
 
 ## Plot for CNV burden split by CNV frequency
@@ -208,20 +208,20 @@ rare$Pvalue <- as.numeric(rare$Pvalue)
 rare$Pvalue <- signif(ifelse(rare$OR>1, rare$Pvalue/2, 1-(rare$Pvalue/2)),3)
 
 p2 <- ggplot(data=rare, aes(x=OR, y=CNV_Count)) +
-  geom_errorbar(aes(xmin=lower, xmax = upper), width=0.2, linewidth = 6, color="#AF58BA") +
-  geom_point(size=12, color="#AF58BA")  +
+  geom_errorbar(aes(xmin=lower, xmax = upper), width=0.2, linewidth = 8, color="purple") +
+  geom_point(size=16, color="purple")  +
   labs(y='UKBB rCNV Count', x = 'OR', tag = "A") +
   geom_vline(xintercept=1, color='black', linetype='dashed', alpha=2, linewidth=1.5) +
   scale_y_discrete(limits = c("Singleton", "2-5", "6-10", "11-100", "101-1000","1001-1500", "1501-3872")) +
-  theme_minimal(base_size=110) +
+  theme_minimal(base_size=120) +
   xlim(c(0.7, 1.3))
 
 
 ## Print plots
 all_plots <- ggarrange(p2, p1, 
-                       ncol = 2, widths=c(2,3))
+                       ncol = 2, widths=c(1.8,3))
 
-png(filename = paste(wkdir, "CNV_burden_plot.png", sep="/"),width = 80, height = 30, units='in', bg="white", res=300, type=c("cairo"))
+png(filename = paste(wkdir, "CNV_burden_plot.png", sep="/"),width = 80, height = 35, units='in', bg="white", res=300, type=c("cairo"))
 print(all_plots)
 dev.off()
 
